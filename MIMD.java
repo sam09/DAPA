0c5b4e7afcdba60f1b408fb757f2270341458e20
Sam Radhakrishnan <sk09idm@gmail.com>
2016-04-09 05:57:35 +0530

    Add MIMD Model

--- MIMD.java
+++ MIMD.java
@@ -2,12 +2,14 @@ import java.util.*;
 import java.io.*;
 
 
-public class SIMD
+public class MIMD
 {
 	final int no_procs;
 	public double a;
 	public double b;
 	public double c;
+	public int r;
+	public double root;
 	public double[] value = new double[2];
 	
 	public double f(double x)
@@ -18,81 +20,69 @@ public class SIMD
 	{
 		return Math.cos(x);
 	}
-	public SIMD()
+	public MIMD()
 	{
 		a = 0;
 		b = 100;
 		no_procs = 10;
 		c = 0.000001;
+		r = 100;
+		root = -1;
 	}
 	public static void main( String args[])
 	{
-		SIMD S = new SIMD();
-		S.start();
-		System.out.println(S.a);
+		MIMD M = new MIMD();
+		M.start();
 	}
 	public void start()
 	{
 		double s;
-		double[] y = new double[no_procs+2];
 		Thread[] t = new Thread[no_procs];
-		while ( (b - a) >= c ) 
+		s = (b - a)/(no_procs + 1);
+			
+		for(int i = 1; i<=no_procs; i++)
 		{
-			s = (b - a)/(no_procs + 1);
-			y[0] = f(a);
-			y[no_procs+1] = f(b);
-			for(int i = 1; i<=no_procs; i++)
-			{
-				t[i-1] = new Thread( new Handler(i, a, b, s) );
-				t[i-1].start();
-			}
-			for (int i=0; i<no_procs ; i++ )
-			{
-				try 
-				{
-					t[i].join();
-				}
-				catch ( InterruptedException e)
-				{
-					System.exit(1);
-				}
-			}
-			if( y[no_procs] * y[no_procs+1] < 0)
-				value[0] = a + no_procs * s;
-
-			a = value[0];
-			b = value[1];
-			System.out.println(a);
-			System.out.println(b);
+			t[i-1] = new Thread( new Handler(i, s) );
+			t[i-1].start();
 		}
+		for (int i=0; i<no_procs ; i++ )
+		{
+			try 
+			{
+				t[i].join();
+			}
+			catch ( InterruptedException e)
+			{
+				System.exit(1);
+			}
+		}
+		
+		System.out.println(root);
 		
 	}
 	class Handler extends Thread
 	{
 		public int index;
-		public double a;
-		public double b;
-		public double s;
 		public double curr;
 		public double prev;
-		Handler(int i, double a, double b, double s)
+		public double s;
+		Handler(int i, double s)
 		{
 			index = i;
-			this.a = a;
-			this.b = b;
 			this.s = s;
 		}
 		public void run()
 		{
-			curr = f(a + index * s);
-			prev = f(a + index*s - s);
-			if (curr * prev < 0)
+			prev = f(a + index * s);
+			int iteration = 0;
+			while( iteration < r && root == -1)
 			{
-				a = a + (index -1 ) * s;
-				b = a + index * s;
+				curr = prev - f(prev) / fprime(prev);
+				if (Math.abs(curr - prev) < c)
+					root = curr;
+				prev = curr;
+				iteration++;
 			}
-			value[0] = a;
-			value[1] = b;
 		}
 	}
 }
